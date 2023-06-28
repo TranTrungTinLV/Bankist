3a21accc16f1b76e7ce38423add6e58448e7c04f
TranTrungTinLV <74409780+TranTrungTinLV@users.noreply.github.com>
2023-06-28 11:30:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,21 +6,21 @@
 
 // Data
 const account1 = {
-    owner: 'Jonas Schmedtmann',
+    owner: 'Le Minh Huy',
     movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
     interestRate: 1.2, // %
     pin: 1111,
 };
 
 const account2 = {
-    owner: 'Jessica Davis',
+    owner: 'Vo Nhat Anh',
     movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
     interestRate: 1.5,
     pin: 2222,
 };
 
 const account3 = {
-    owner: 'Steven Thomas Williams',
+    owner: 'Tran Trung Tin',
     movements: [200, -200, 340, -300, -20, 50, 400, -460],
     interestRate: 0.7,
     pin: 3333,
